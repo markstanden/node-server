8f8377e316940a82478ccb3db26025ebfa031c31
Mark Standen <mark@markstanden.uk>
2021-02-21 15:47:22 +0000

    initial auth layout

--- build/controllers/authentication.js
+++ build/controllers/authentication.js
@@ -2,6 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.signup = void 0;
 function signup(req, res, next) {
-    res.send({ success: 'true' });
+    console.log(req.body);
+    // See if a user with the given email address exists
+    // If a user with email address does exist, return an error
+    // If a user with email doesn't exist, create and save user record
+    // Respond to request indicating the user was created
 }
 exports.signup = signup;
--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -1,5 +1,14 @@
 import express from "express"
 
 export function signup (req: express.Request, res: express.Response, next: express.NextFunction) {
-  res.send({ success: 'true' })
+  console.log (req.body)
+  // See if a user with the given email address exists
+  
+  // If a user with email address does exist, return an error
+
+  // If a user with email doesn't exist, create and save user record
+
+  // Respond to request indicating the user was created
+
 }
+
